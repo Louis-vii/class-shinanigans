57dc91ea08b56fe2796da017a09009614bfb893b
louis <louisxiv831@gmail.com>
2025-10-07 19:56:22 +0200

    adiino

--- glorp/src/lovergirl.java
+++ glorp/src/lovergirl.java
@@ -3,5 +3,12 @@ import java.util.*;
 public class lovergirl {
   public static void main(String[] args) {
 
+    for (int i = 1; i > 0; i++)
+      cumblaster();
   }
+
+  public static void cumblaster() {
+    System.out.println("balls");
+  }
+
 }
