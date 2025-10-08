ab7a393bee5020958a0d50c3731c5aa0bb2edfb3
louis <louisxiv831@gmail.com>
2025-10-08 23:47:59 +0200

    adiido

--- python/main.py
+++ python/main.py
@@ -1,5 +1,14 @@
-x = 0
-x = "balls"
-x = 88
+player_level = 4
 
-print(x)
+
+def calculate_health(modifier):
+    return player_level * modifier
+
+
+def calculate_primary_stats(armor_bonus, modifier):
+    return armor_bonus + modifier + player_level
+
+
+print(f"Character has {calculate_health(10)} max health.")
+
+print(f"Character has {calculate_primary_stats(3, 8)} primary stats.")
